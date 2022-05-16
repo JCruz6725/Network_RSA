b406d744b9a5fcf61ed2a3cb0a57a2011109f229
JohnCruz <john.cruz6725@gmail.com>
2022-05-16 16:16:12 -0500

    rework of the RSA-added a class system aswell as big prime generations. as well as started doc on google drive

--- My_RSA.py
+++ My_RSA.py
@@ -1,29 +1,130 @@
 import random
 from string_functions import *
 import json
+import time
+from math import gcd
+
+#import Thread from threading
 #random.seed(20)
 
 class RSA():
-    def __init__ (self, upper = 2**8, lower = 2**5):
-        self.__lower = lower
-        self.__upper = upper
-        self.__keys = self.__set_keys(self.__lower, self.__upper)
+    def __init__ (self):
+    
+        self.primes = self.sieve_for_primes_to(2**16)      
+        self.p = self.get_big_prime()
+        self.q = self.get_big_prime()        
+        self.n = self.p * self.q
+        self.totient_var = self.__totient(self.p, self.q)
+     
+        self.e = -1
+        print ("getting E")
+        
+        while (self.e == -1 or self.e > self.totient_var or
+                gcd(self.e, self.totient_var) != 1):
+            self.e = self.get_big_prime()
+         
+        print (f"{gcd(self.e, self.totient_var)}"  )
+        
+     
+     
+     
+    def get_big_prime(self, test_itteration = 500):
+        number = -1
+        while (number == -1):
+            number = self.__random_odd_int(2**1023, 2**1024)
+            #if (self.square_root_test(number)):
+            if (self.miller_rabin_test(number, test_itteration)):
+                prime = number
+                return prime
+            else:
+                number = -1
+                
+    
+    #################################
+
+    def square_root_test(self, large_prime):
+        
+        for prime in self.primes:
+            if (large_prime % prime == 0):
+                return False
+        return True
+
+    ########################
+    
+    def miller_rabin_test(self, n, k):
+        #Source ----> https://gist.github.com/Ayrx/5884790
+        # Implementation uses the Miller-Rabin Primality Test
+        # The optimal number of rounds for this test is 40
+        # See http://stackoverflow.com/questions/6325576/how-many-iterations-of-rabin-miller-should-i-use-for-cryptographic-safe-primes
+        # for justification
+
+        # If number is even, it's a composite number
+
+        if (n == 2):
+            return True
+
+        if (n % 2 == 0):
+            return False
+
+        r, s = 0, n - 1
+        while (s % 2 == 0):
+            r += 1
+            s //= 2
+        for _ in range(k):
+            a = random.randrange(2, n - 1)
+            x = pow(a, s, n)
+            if (x == 1 or x == n - 1):
+                continue
+            for _ in range(r - 1):
+                x = pow(x, 2, n)
+                if (x == n - 1):
+                    break
+            else:
+                return False
+        return True
+
+    ########################
+    
+    
+    
+    def sieve_for_primes_to(self, n):
+        #stack overflow.... 
+        #https://stackoverflow.com/questions/16004407/a-fast-prime-number-sieve-in-python/18997575#18997575
+        size = n//2
+        sieve = [1]*size
+        limit = int(n**0.5)
+        for i in range(1,limit):
+            if sieve[i]:
+                val = 2*i+1
+                tmp = ((size-1) - i)//val 
+                sieve[i+val::val] = [0]*tmp
+        return [2] + [i*2+1 for i, v in enumerate(sieve) if v and i>0]
+    
+    
+        
     
     def __random_int(self, lower_limit, upper_limit):
         r = random.randint(lower_limit, upper_limit)
         return r
-
+        
+    def __random_odd_int(self, lower_limit, upper_limit):
+        r_number = 2
+        while (r_number%2 == 0):
+            r_number = random.randint(lower_limit, upper_limit)
+        return r_number    
+        
 
     #################################################
     # math functions to get public and private keys #
     #################################################
-
+    
     def __mod_exponentiation(self, char_val, key, tot_n):
         remainder = (char_val**key)%(tot_n)
         return remainder
 
 
     def __check_if_prime(self, n):
+        #Remove this
         if (n <= 1):
             return False
         else:
@@ -34,11 +135,15 @@ class RSA():
 
 
     def __get_primes_in_range (self, x, y):
+        #Remove this
         primes = []
         for i in range (x, y):
             if (self.__check_if_prime(i)):
                 primes.append(i)
         return primes
+       
+
+        
 
         
     def __get_p_and_q(self, lower_limit, upper_limit):
--- test.py
+++ test.py
@@ -26,14 +26,14 @@ for i in range (0, 20):
 print ('********************')
 '''
 
-rsa = RSA(upper = 1024, lower = 0)
-string_t = 'hello'
-cypher = rsa.encrypt(string_t, rsa.get_keys())
-keys = rsa.get_keys()
-print (cypher)
-print (keys)
+IT = 500
 
-print ('')
-pt = rsa.decrypt(cypher, rsa.get_keys())
-print (f'decrypt: "{pt}" ')
+a = time.time()
+r = RSA()
+print(time.time() - a)
 
+print()
+
+print(f"{r.totient_var} \n \n ")
+
+ 
